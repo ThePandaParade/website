df6a88010968262ab02cdf6edc6c39c199df8d5d
Nxybi <thomaswbrittain67@gmail.com>
2020-07-12 22:25:43 +0100

    new website

--- main.js
+++ main.js
@@ -4,16 +4,21 @@ const efu = require("express-fileupload")
 var crypto = require("crypto")
 var filet = require("file-type")
 const readChunk = require('read-chunk');
+const path = require('path');
 
 app.use(express.static("images"))
+app.use(express.static("static"))
 app.use(efu({safeFileNames: true}))
 
 app.get('/', (req, res) => {
-    res.send("Yiff is hot")
+    if ("images" in req.subdomains) {
+        return res.status(418).send("418: You're a teapot.<br><a href=\"https://nxybi.me/\">You're looking for here</a>")
+    }
+    res.sendFile(path.join(__dirname, "/web/root.html"))
 })
 
 app.post('/upload', (req, res) => {
-    if (req.headers.authorization !== "CDyrnblrmxwk2TTkOOVRC8cSsGKAJFxp66uHYi9SEj4wS9M2zD") {
+    if (req.headers.authorization !== "48f200B1eA732c8EaEc729C34189e7F76cBe9F2840dCcEdEcA") {
         return res.status(401).send({'error': true, 'code': 401})
     }
     
@@ -29,7 +34,7 @@ app.post('/upload', (req, res) => {
 
     const flname = crypto.randomBytes(6).toString("hex")
     uploadImg.mv(`images/${flname}.${exte}`)
-    res.send(`https://${req.hostname}/${flname}.${exte}`)
+    res.send(`https://images.nxybi.me/${flname}.${exte}`)
 })
 
 app.listen("6969", () => console.log("Image server running on port 6969"))
