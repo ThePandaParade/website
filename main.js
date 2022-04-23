f3404b5970d0d2b85ddb340283eb422282e655e8
Nxybi <thomaswbrittain67@gmail.com>
2022-04-23 18:54:42 +0100

    [crying]

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var filet = require("file-type")
 const readChunk = require('read-chunk');
 const path = require('path');
 const fs = require("fs")
-const rawsubdom = require("./domains.json")
+const rawsubdom = Object.keys(require("./domains.json"))
 
 
 app.use(express.static("images"))
@@ -14,17 +14,19 @@ app.use(express.static("static"))
 app.use(efu({safeFileNames: true}))
 
 app.get('/', (req, res) => {
-    if (req.subdomains[0] == "images") {
-        return res.status(418).send("418: You're a teapot.<br><a href=\"https://nxybi.me/\">You're looking for here</a>")
+    if (req.subdomains[0] || !req.url.startsWith("https://nxybi.me/")) {
+        return res.status(418).send()
     }
-    if (req.hostname == "yiffing.life") {
-        return res.redirect("https://nxybi.me/")
-    }
+
     res.sendFile(path.join(__dirname, "/web/root.html"))
 })
 
+app.get('/devices', (req,res) => {
+    res.sendFile(path.join(__dirname, "/web/devices.html"))
+})
+
 app.post('/upload', (req, res) => {
-    if (req.headers.authorization !== "48f200B1eA732c8EaEc729C34189e7F76cBe9F2840dCcEdEcA") {
+    if (req.headers.authorization !== process.env.IMAGE_SERVER) {
         return res.status(401).send({'error': true, 'code': 401})
     }
     
@@ -35,12 +37,15 @@ app.post('/upload', (req, res) => {
     let uploadImg = req.files.upImg
     let ogName = uploadImg.name
     const buffer = uploadImg.data.slice(0, filet.minimumBytes); 
-
     let exte = filet(buffer).ext
-
-    const flname = crypto.randomBytes(6).toString("hex")
-    uploadImg.mv(`images/${flname}.${exte}`)
-    res.send(`https://the.yiffing.life/${flname}.${exte}`)
+    let chosenDomain = rawsubdom[rawsubdom.length * Math.random() << 0]
+    ogName = ogName.replace(exte,"")
+    // const flname = crypto.randomBytes(6).toString("hex")
+    // uploadImg.mv(`images/${flname}.${exte}`)
+    // res.send(`https://the.yiffing.life/${flname}.${exte}`)
+    
+    uploadImg.mv(`images/${ogName}.${exte}`)
+    res.send(`https://${chosenDomain}/${ogName}.${exte}`)
 })
 
 app.listen("6969", () => console.log("Image server running on port 6969"))
