506690af85728fda7ce33896fbcb3c918a47ef9c
WhenDawnEnds <thomaswbrittain67@gmail.com>
2023-08-12 14:47:43 +0100

    We in the future. Redesign finalised and published

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ const efu = require("express-fileupload")
 var filet = require("file-type")
 const path = require('path');
 const rawsubdom = Object.keys(require("./domains.json"))
+const geoip = require("geoip-lite")
+const embargo = [
+    // Embargoed countries, subdivisions or cities. (https://en.wikipedia.org/wiki/Embargo#List_of_countries_under_embargo)
+    // This can also be regions under sanctions (https://en.wikipedia.org/wiki/International_sanctions#Sanctioned_countries), typically diplomatic/military sanctions.
+    // This is a list of countries that we will block from accessing any of our services.
+    // This is because I am UK-based, and I do not want to be breaking any laws, or be in any legal trouble.
+    // Especially since I do plan on hosting a shop on this domain later on.
 
-const NodeCache = require('node-cache');
-const fetch = require('node-fetch-commonjs');
-const cacheDurationSeconds = 60 * 60; // 1 hour in seconds
-const weatherCache = new NodeCache({
-    stdTTL: cacheDurationSeconds,
-    checkperiod: cacheDurationSeconds * 0.2,
-    useClones: false
-});
-
-// For /time, cache the data for 1 minute
-const timeCache = new NodeCache({
-    stdTTL: 60,
-    checkperiod: 60 * 0.2,
-    useClones: false
-});
+    // Format: ISO 3166-1 alpha-2 country code (https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)
+    "RF", // Russia
+    "KP", // North Korea
+    "IR", // Iran
+    "CN", // China (People's Republic of)
+    // Last updated: 2023-05-18
+    // If a legal representative wants to add/remove a country to this list, please contact me via email (robyn@pandapa.ws). 
+]
 
 if (process.env.MODE == "PRODUCTION" && process.env.MODE == "DEVELOPMENT") {
     throw new Error("Invalid Proccess Enviorment Mode; Aborting...")
@@ -30,7 +30,25 @@ app.use(express.static("images"))
 app.use(express.static("static"))
 app.use(efu({safeFileNames: true}))
 
-app.get('/', (req, res) => {
+// Geoblocking \\
+app.use((req, res, next) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const country = geoip.lookup(ip)?.country;
+    const region = geoip.lookup(ip)?.region;
+    const city = geoip.lookup(ip)?.city;
+    if ((embargo.includes(country) || embargo.includes(region) || embargo.includes(city)) && req.url !== '/bleeding/error/451') {
+        console.log(`Blocked request from ${country} - ${ip}`)
+        res.redirect('/bleeding/error/451');
+    } else {
+        next();
+    }
+});
+
+
+// Redesign \\
+app.use("/",require("./web/redesign/redesign.js"))
+
+/* app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/web/root.html"))
 })
 
@@ -48,7 +66,7 @@ app.get("/links/onlyfans", (req, res) => {
     <meta property="og:description" content="OnlyFans is the social platform revolutionizing creator and fan connections. The site is inclusive of artists and content creators from all genres and allows them to monetize their content while developin...">
     </head> <script> window.location.replace("https://www.youtube.com/watch?v=xm3YgoEiEDc") </script>
     `)
-})
+}) */
 
 app.get("/links/twitter", (req, res) => {
     res.redirect("https://twitter.com/WhenDawnEnds")
@@ -81,7 +99,6 @@ app.get("/links/projects/twb", (req, res) => {
 
 // Bleeding Edge \\
 // Redesign Zone \\
-app.use("/bleeding",require("./web/redesign/redesign.js"))
 
 app.get("/notice/", (req, res) => {
     res.sendFile(path.join(__dirname, "/web/notice.html"))
@@ -89,118 +106,7 @@ app.get("/notice/", (req, res) => {
 
 
 
-// Weather & Time \\
-app.get("/weather", (req, res) => {
-    // Make a call to OpenWeatherMap API to get the weather
-    // This should use the town defined in the .env file, but if it's not defined, it should default to "London"
-    // The API key should be defined in the .env file
 
-    const town = process.env.TOWN || 'London';
-    const apiKey = process.env.OPENWEATHERMAP_API_KEY;
-    //console.log(town, apiKey)
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${town}&appid=${apiKey}`;
-
-    // Check if weather data is cached
-    const cachedData = weatherCache.get(apiUrl);
-    if (cachedData) {
-        //console.log(`Serving cached weather data for ${town}`);
-        return res.json(cachedData);
-    }
-
-    // Otherwise, fetch new weather data and cache it
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            //console.log(`Fetching fresh weather data for ${town}`);
-            // JSONify the data and send it to the client
-            const pre = JSON.parse(JSON.stringify(data));
-            // Data for handling the icon from OpenWeatherMap.
-            // The icon is a string like "01d" or "04n", which we need to convert to a FontAwesome icon name
-            switch (pre.weather[0].icon) {
-                case "01d":
-                    pre.weather[0].icon = "fas fa-sun";
-                    break;
-                case "01n":
-                    pre.weather[0].icon = "fas fa-moon";
-                    break;
-                case "02d":
-                case "03d":
-                    pre.weather[0].icon = "fas fa-cloud-sun";
-                    break;
-                case "02n":
-                case "03n":
-                    pre.weather[0].icon = "fas fa-cloud-moon";
-                    break;
-                case "04d":
-                case "04n":
-                    pre.weather[0].icon = "fas fa-cloud";
-                    break;
-                case "09d":
-                case "09n":
-                    pre.weather[0].icon = "fas fa-cloud-rain";
-                    break;
-                case "10d":
-                case "10n":
-                    pre.weather[0].icon = "fas fa-cloud-showers-heavy";
-                    break;
-                case "11d":
-                case "11n":
-                    pre.weather[0].icon = "fas fa-bolt";
-                    break;
-                case "13d":
-                case "13n":
-                    pre.weather[0].icon = "fas fa-snowflake";
-                    break;
-                case "50d":
-                case "50n":
-                    pre.weather[0].icon = "fas fa-smog";
-                    break;
-            }
-            const form = {
-                // Round the temperature to the nearest integer
-                "temp": Math.round(pre.main.temp),
-                "weather": pre.weather[0].main,
-                "icon": pre.weather[0].icon
-            }
-            weatherCache.set(apiUrl, form);
-            res.json(form);
-        })
-        .catch(error => console.error(error));
-});
-
-app.get("/time", (req, res) => {
-    // Make a call to WorldTime API to get the time
-    // Get the time for London.
-    // The API key should be defined in the .env file
-
-    const apiUrl = `http://worldtimeapi.org/api/timezone/Europe/London`;
-
-    // Check if time data is cached
-    const cachedData = timeCache.get(apiUrl);
-    if (cachedData) {
-        //console.log(`Serving cached time data for London`);
-        return res.json(cachedData);
-    }
-
-    // Otherwise, fetch new time data and cache it
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            //console.log(`Fetching fresh time data for London`);
-
-            // Send the data as the response
-            const pre = JSON.parse(JSON.stringify(data));
-            const form = {
-                "abbreviation": pre.abbreviation,
-                "hour": new Date(pre.datetime).getHours(),
-                "minute": new Date(pre.datetime).getMinutes(),
-            }
-            // Cache the data for later use
-            timeCache.set(apiUrl, form);
-            res.json(form);
-        })
-        .catch(error => {console.error(error); res.status(500).send({error: true, code: 500, message: "Internal Server Error"})});
-})
 
 // Deprecated Zone \\
 app.post('/upload', (req, res) => {
--- web/redesign/redesign.js
+++ web/redesign/redesign.js
@@ -5,6 +5,22 @@ require('dotenv').config()
 const app = express.Router()
 const path = require('path');
 
+const NodeCache = require('node-cache');
+const fetch = require('node-fetch-commonjs');
+const cacheDurationSeconds = 60 * 60; // 1 hour in seconds
+const weatherCache = new NodeCache({
+    stdTTL: cacheDurationSeconds,
+    checkperiod: cacheDurationSeconds * 0.2,
+    useClones: false
+});
+
+// For /time, cache the data for 1 minute
+const timeCache = new NodeCache({
+    stdTTL: 60,
+    checkperiod: 60 * 0.2,
+    useClones: false
+});
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/index.html"))
 })
@@ -42,25 +58,135 @@ app.use("/avatar/ascii", (req, res) => {res.set("Content-Type", "image/png"); re
 app.use("/art/blankie", (req, res) => {res.set("Content-Type", "image/png"); res.sendFile(path.join(__dirname, "/static/blankie.png"));})
 app.use("/avatar/normal", (req, res) => {res.set("Content-Type", "image/jpg"); res.sendFile(path.join(__dirname, "/static/avatar.jpg"));})
 app.use("/terminal.js", (req, res) => {res.set("Content-Type", "text/javascript"); res.sendFile(path.join(__dirname, "/static/terminal.js"));})
+app.use("/art/comfy", (req, res) => {res.set("Content-Type", "image/png"); res.sendFile(path.join(__dirname, "/static/comf.png"));})
+app.use("/branding/banner", (req, res) => {res.set("Content-Type", "image/png"); res.sendFile(path.join(__dirname, "/static/banner.png"));})
+
+// Weather & Time \\
+app.get("/internal/weather", (req, res) => {
+    // Make a call to OpenWeatherMap API to get the weather
+    // This should use the town defined in the .env file, but if it's not defined, it should default to "London"
+    // The API key should be defined in the .env file
+
+    const town = process.env.TOWN || 'London';
+    const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+    //console.log(town, apiKey)
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${town}&appid=${apiKey}`;
+
+    // Check if weather data is cached
+    const cachedData = weatherCache.get(apiUrl);
+    if (cachedData) {
+        //console.log(`Serving cached weather data for ${town}`);
+        return res.json(cachedData);
+    }
+
+    // Otherwise, fetch new weather data and cache it
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            //console.log(`Fetching fresh weather data for ${town}`);
+            // JSONify the data and send it to the client
+            const pre = JSON.parse(JSON.stringify(data));
+            // Data for handling the icon from OpenWeatherMap.
+            // The icon is a string like "01d" or "04n", which we need to convert to a FontAwesome icon name
+            switch (pre.weather[0].icon) {
+                case "01d":
+                    pre.weather[0].icon = "fas fa-sun";
+                    break;
+                case "01n":
+                    pre.weather[0].icon = "fas fa-moon";
+                    break;
+                case "02d":
+                case "03d":
+                    pre.weather[0].icon = "fas fa-cloud-sun";
+                    break;
+                case "02n":
+                case "03n":
+                    pre.weather[0].icon = "fas fa-cloud-moon";
+                    break;
+                case "04d":
+                case "04n":
+                    pre.weather[0].icon = "fas fa-cloud";
+                    break;
+                case "09d":
+                case "09n":
+                    pre.weather[0].icon = "fas fa-cloud-rain";
+                    break;
+                case "10d":
+                case "10n":
+                    pre.weather[0].icon = "fas fa-cloud-showers-heavy";
+                    break;
+                case "11d":
+                case "11n":
+                    pre.weather[0].icon = "fas fa-bolt";
+                    break;
+                case "13d":
+                case "13n":
+                    pre.weather[0].icon = "fas fa-snowflake";
+                    break;
+                case "50d":
+                case "50n":
+                    pre.weather[0].icon = "fas fa-smog";
+                    break;
+            }
+            const form = {
+                // Round the temperature to the nearest integer
+                "temp": Math.round(pre.main.temp),
+                "weather": pre.weather[0].main,
+                "icon": pre.weather[0].icon
+            }
+            weatherCache.set(apiUrl, form);
+            res.json(form);
+        })
+        .catch(error => console.error(error));
+});
+
+app.get("/internal/time", (req, res) => {
+    // Make a call to WorldTime API to get the time
+    // Get the time for London.
+    // The API key should be defined in the .env file
+
+    const apiUrl = `http://worldtimeapi.org/api/timezone/Europe/London`;
+
+    // Check if time data is cached
+    const cachedData = timeCache.get(apiUrl);
+    if (cachedData) {
+        //console.log(`Serving cached time data for London`);
+        return res.json(cachedData);
+    }
+
+    // Otherwise, fetch new time data and cache it
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            //console.log(`Fetching fresh time data for London`);
+
+            // Send the data as the response
+            const pre = JSON.parse(JSON.stringify(data));
+            const form = {
+                "abbreviation": pre.abbreviation,
+                "hour": new Date(pre.datetime).getHours().toString().padStart(2, "0"),
+                "minute": new Date(pre.datetime).getMinutes().toString().padStart(2, "0")
+            }
+            // Cache the data for later use
+            timeCache.set(apiUrl, form);
+            res.json(form);
+        })
+        .catch(error => {console.error(error); res.status(500).send({error: true, code: 500, message: "Internal Server Error"})});
+})
 
 // 404 handler
 app.use((req, res, next) => {
     // Render the 404 page
     // However, if this is a request to any url with "/error/" in it, then don't redirect to the 404 page
-    // And if this is a request to any url with "/bleeding/" in it, then redirect to /bleeding/error/404/
+    // And if this is a request to any url with "/" in it, then redirect to /error/404/
     if (req.url.includes("/error/") || req.url.includes("/maintenance")) {
         next()
-    } else if (req.url.includes("/bleeding/")) {
+    } else if (req.url.includes("/bleeding")) {
        res.redirect("/bleeding/error/404/")
     } else {
-        // Until I actually finish the redesign, I'm going to redirect to /bleeding/error/404/
-        res.redirect("/bleeding/error/404/")
+        // Until I actually finish the redesign, I'm going to redirect to /error/404/
+        res.redirect("/error/404/")
     }
-})
-
-app.use((err, req, res, next) => {
-    // Render the error page
-    res.redirect("/bleeding/error/" + err.status)
 })
 
 // Error handler
@@ -68,12 +194,13 @@ app.use((err, req, res, next) => {
     try {
         // Render the error page
         if (err.status == undefined) {
-            res.redirect("/bleeding/error/500/") // If this happens its a big oopsie
+            res.redirect("/error/500/") // If this happens its a big oopsie
         }
-        res.redirect("/bleeding/error/" + err.status)
+        res.status(err.status.toNumber())
+        res.redirect("/error/" + err.status)
     } catch (err) {
         console.log(err)
-        res.redirect("/bleeding/error/500/") 
+        res.redirect("/error/500/") 
     }
 })
 
